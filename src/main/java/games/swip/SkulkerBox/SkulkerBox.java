4128b491046912eca373bf80e5f7fbf42aa342c0
EysseW <eyssew@gmail.com>
2026-04-04 17:09:39 +0200

    name change

--- src/main/java/games/swip/SkulkerBox/SkulkerBox.java
+++ src/main/java/games/swip/SkulkerBox/SkulkerBox.java
@@ -1,11 +1,11 @@
-package com.skulkerbox;
+package games.swip.SkulkerBox;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class SkulkerBox implements ModInitializer {
 	public static final String MOD_ID = "modid";
 
 	// This logger is used to write text to the console and the log file.
