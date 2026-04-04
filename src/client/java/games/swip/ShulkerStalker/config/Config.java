f45bbb9e0442965c6f02664ab648184e5cbdb300
EysseW <eyssew@gmail.com>
2026-04-04 22:22:27 +0200

    version bump

--- src/client/java/games/swip/ShulkerStalker/config/Config.java
+++ src/client/java/games/swip/ShulkerStalker/config/Config.java
@@ -5,5 +5,5 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class Config extends MidnightConfig {
 	public static final String GENERAL = "General";
 	@Entry(category = GENERAL, name= "Enabled") public static boolean enabled = true;
-	@Entry(category = GENERAL, name = "Time to pickup shulkerbox") public static int timer = 400;
+	@Entry(category = GENERAL, name = "Time to pickup shulkerbox") public static int timer = 100;
 }
