f99ae60411ce3d14d8bf8a3ad95f94e2439dcd99
EysseW <eyssew@gmail.com>
2026-04-04 17:32:50 +0200

    working block break detection

--- src/client/java/games/swip/SkulkerBox/mixin/client/SkulkerBoxMixin.java
+++ src/client/java/games/swip/SkulkerBox/mixin/client/SkulkerBoxMixin.java
@@ -1,15 +1,17 @@
 package games.swip.SkulkerBox.mixin.client;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Minecraft.class)
+@Mixin(MultiPlayerGameMode.class)
 public class SkulkerBoxMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
+	@Inject(at = @At("HEAD"), method = "destroyBlock")
+	private void init(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
 		// This code is injected into the start of Minecraft.run()V
+		System.out.println("Local player mined block at: " + pos.toShortString());
 	}
 }
