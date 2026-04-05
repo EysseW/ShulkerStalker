6068277cabac4dbb466180f1f15818cf5cf607dc
EysseW <eyssew@gmail.com>
2026-04-05 04:49:23 +0200

    Cleared up some config descriptions

--- src/client/java/games/swip/ShulkerStalker/config/Config.java
+++ src/client/java/games/swip/ShulkerStalker/config/Config.java
@@ -5,9 +5,9 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class Config extends MidnightConfig {
 	public static final String GENERAL = "General";
 	@Entry(category = GENERAL, name= "Enabled") public static boolean enabled = true;
-	@Entry(category = GENERAL, name = "Time to pickup shulkerbox") public static int initial_timer = 100;
+	@Entry(category = GENERAL, name = "Time before warning") public static int initial_timer = 100;
+	@Entry(category = GENERAL, name = "Time in between reminders") public static int remind_timer = 20;
 	@Entry(category = GENERAL, name = "Apply glowing to dropped item") public static boolean apply_glowing = true;
 	@Entry(category = GENERAL, name = "Enable warning sound") public static boolean warning_sound = true;
-	@Entry(category = GENERAL, name = "Remind timer") public static int remind_timer = 20;
-	@Entry(category = GENERAL, name = "Flash message") public static boolean flash_mesage = false;
+	@Entry(category = GENERAL, name = "Flash warning message") public static boolean flash_mesage = false;
 }
